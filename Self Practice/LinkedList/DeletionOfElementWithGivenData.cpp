fb6f4682607c2339f1d40b2ee698728a41df722b
Ayush <ayushmaurya2050@gmail.com>
2023-07-31 23:12:52 +0530

    edited few files

---
+++ Self Practice/LinkedList/DeletionOfElementWithGivenData.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+class node{
+
+    public:
+    int val; node* next;
+    
+    node(){
+        val=0; next=NULL;
+    }
+    node(int n){
+        val=n; next=NULL;
+    }
+
+};
+
+void print(node* head){
+
+    while (head!=NULL){
+        cout << head->val << " ";
+        head=head->next;
+    }
+    cout << endl;
+
+}
+
+void insertionAtPosition(node* &head, int pos, int n){
+
+    node* temp = new node(n);
+    if (pos==1){
+        temp->next=head;
+        head=temp;
+        return;
+    }
+    int i=1;
+    node* p=head;
+    while (i+1!=pos){
+        head=head->next; i++;
+    }
+    if (head->next==NULL){
+        head->next=temp;
+    } else {
+        temp->next=head->next;
+        head->next=temp;
+    }
+    head=p;
+
+}
+
+void deletionOfVal(node* head, int n){
+
+    
+
+}
+
+int main(){
+    
+    node* head = new node(1);
+    insertionAtPosition(head, 1, 10);
+    insertionAtPosition(head, 1, 2);
+    insertionAtPosition(head, 2, 3);
+    insertionAtPosition(head, 4, 100);
+    insertionAtPosition(head, 3, 99);
+    insertionAtPosition(head, 7, 90);
+    print(head);
+    
+    return 0;
+}
